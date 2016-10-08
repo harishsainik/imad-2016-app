4f78bdbc63a74c2fe628121afefb63cf41a46935
harishsainik <harishsaini.lajak@gmail.com>
2016-10-07 22:41:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,5 @@ console.log('Loaded!');
 //changing the text
 var element=document.getElementById('id1');
 element.innerHTML="<h1> Hii there the text has been changed by javascript</h1>";
+var img=document.getElementById('img-id');
+img.style.marginLeft="100px";
