e8d137923cfbb97605524e18cf57a0dd1348203f
harishsainik <harishsaini.lajak@gmail.com>
2016-10-07 23:14:39 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,13 @@ console.log('Loaded!');
 var element=document.getElementById('id1');
 element.innerHTML="<h1> Hii there the text has been changed by javascript</h1>";
 var img=document.getElementById('img-id');
-var pos=-600;
+var pos=-1000;
 function shiftRight(){
   pos+=2;
   img.style.marginLeft=pos+'px';
-  if(pos>600)
+  if(pos>1000)
   {
-      pos=-600;
+      pos=-1000;
   }
 }
 img.onclick=function(){
